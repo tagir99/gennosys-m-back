bd696ff45ad7550036c4d907667a3fee35ad1778
unknown <tagir9980@gmail.com>
2023-08-26 12:51:09 +0300

    add backend files-RU

---
+++ README.md
@@ -0,0 +1,1 @@
+"# gennosys-m-back" 
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator'
+
+export class AuthDto {
+	@IsEmail()
+	email: string
+
+	@MinLength(6, {
+		message: 'Password must be at least 6 characters long'
+	})
+	@IsString()
+	password: string
+
+	@IsOptional()
+	@IsString()
+	name?: string
+}
